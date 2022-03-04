8f60ce20e69faa3e86d656937766126709ed1869
Aakanksha <Aakanksha2013073@akgec.ac.in>
2022-03-04 22:38:16 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,104 @@
+require('dotenv').config();
+const express= require("express");
+const app=express();
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const multer  = require('multer');
+const ejs= require('ejs');
+app.set('view engine','ejs');
+const PORT = process.env.PORT || 7000;
+app.use (express.urlencoded ({extended:true}));
+var port=7000;
+const upload = multer({ dest: 'uploads/'})
+mongoose.connect("mongodb://localhost:27017/crud");
+const db = mongoose.connection;
+db.on('error',function(error){
+    console.log('error is :',error);
+});
+db.once('open',function(){
+    console.log('connected to mongodb')
+});
+//for parsering JSON to js object
+app.use(express.json());
+app.use(express.static("uploads"));
+
+// Storage
+const Storage = multer.diskStorage({
+    destination: function(req,file,cb){
+        cb(null,file.originalname)
+    },  
+})
+// const uploads = multer({
+//     storage : Storage
+// }).single('image')
+
+
+
+const Schema = new mongoose.Schema({
+    Name:String,
+    Roll_No:Number,
+    Branch:String,
+    Hobbies:String,
+    image:{
+          data: Buffer,
+          contentType: String
+    } 
+});
+//COLLECTION CREATION
+//Here,User is a name of collection,should be of capital letter bcz it acts as a class->k/a pascal convention
+const User = new mongoose.model("User",Schema);
+
+
+
+app.get("/",function(req,res){
+    res.render("home");
+})
+
+app.get("/getdetails",function(req,res){
+    User.find({},function (err, allDetails){
+        if(err){
+            console.log(err);
+        }else {
+            console.log(allDetails);
+            res.render("index", { details:allDetails});
+        }
+    })
+});
+
+
+
+app.get("/login",(req,res)=>{
+    res.sendFile(__dirname + "/form.html");
+});
+
+app.post("/",upload.single("image"),(req,res) => {
+    let nUser = new User({
+        Name:req.body.name,
+        Roll_No:req.body.number,
+        Branch:req.body.branch,
+        Hobbies:req.body.hobbies,
+        image:{
+             data :req.file,
+             contentType: 'image/png',
+        }
+    });
+    nUser.save();
+    res.send("Saved");
+});
+app.post("/del",function(req,res){
+    console.log(req.body)
+    User.deleteOne({_id : req.body.submit},function(err,result){
+        if(err){
+            console.log(err);
+        }
+        else{
+            res.redirect("/index")
+        }
+    })
+});
+
+ 
+app.listen(port,()=>{
+    console.log("server listening on port" + port);
+
+});
